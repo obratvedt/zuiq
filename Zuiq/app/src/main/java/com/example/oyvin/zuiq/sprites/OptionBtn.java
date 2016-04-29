cd13688e8813090f1179bacdab65e2333c93280e
hermanmk <lex5050@live.no>
2016-04-29 10:18:10 +0200

    Abstractified buttons to their own super class

--- Zuiq/app/src/main/java/com/example/oyvin/zuiq/sprites/OptionBtn.java
+++ Zuiq/app/src/main/java/com/example/oyvin/zuiq/sprites/OptionBtn.java
@@ -1,35 +1,20 @@
 package com.example.oyvin.zuiq.sprites;
 
-
 import android.view.MotionEvent;
 
 import com.example.oyvin.zuiq.ZiuqGame;
 import com.example.oyvin.zuiq.states.ConfigState;
 import com.example.oyvin.zuiq.states.GameModeState;
 
-import sheep.game.Sprite;
 import sheep.graphics.Image;
-import sheep.input.TouchListener;
 
-public class OptionBtn extends Sprite implements TouchListener{
-    float imageWidth, imageHeight;
+public class OptionBtn extends Button {
+
     boolean gameMode;
 
     public OptionBtn(Image image, boolean gameMode){
-        super(image);
-        this.imageHeight = image.getHeight()/2;
-        this.imageWidth = image.getWidth()/2;
-        setScale(0.5f, 0.5f);
+        super(image, 0.5f, 0.5f);
         this.gameMode = gameMode;
-
-    }
-
-    public float getImageWidth() {
-        return imageWidth;
-    }
-
-    public float getImageHeight() {
-        return imageHeight;
     }
 
     @Override
@@ -41,26 +26,7 @@ public class OptionBtn extends Sprite implements TouchListener{
                 return true;
             }
         }
-
         return false;
     }
 
-    @Override
-    public boolean onTouchUp(MotionEvent motionEvent) {
-        return false;
-    }
-
-    @Override
-    public boolean onTouchMove(MotionEvent motionEvent) {
-        return false;
-    }
-
-    private boolean touchOnSprite(float xPos, float yPos) {
-        if (xPos >= getX() - getImageHeight() / 2 && xPos <= getX() + getImageWidth() / 2) {
-            if (yPos >= getY() - getImageHeight() / 2 && yPos <= getY() + getImageHeight() / 2) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
